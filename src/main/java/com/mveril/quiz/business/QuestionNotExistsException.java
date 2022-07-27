c4394638272c6c950ae6b16d2d19c6be3e899188
Mickaël Véril <mika.veril@wanadoo.fr>
2022-07-27 10:16:42 +0200

    QuestionNotExistsException should extends Exception

--- src/main/java/com/mveril/quiz/business/QuestionNotExistsException.java
+++ src/main/java/com/mveril/quiz/business/QuestionNotExistsException.java
@@ -1,6 +1,6 @@
 package com.mveril.quiz.business;
 
-public class QuestionNotExistsException extends Throwable {
+public class QuestionNotExistsException extends Exception {
     public QuestionNotExistsException(long questionId) {
         super(String.format("The question with id: %d not exists",questionId));
     }
